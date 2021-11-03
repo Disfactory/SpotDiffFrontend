73c8a579ccd6b685accb3c5bd7a259debdb45797
DotSea <85677992+DavidFu95@users.noreply.github.com>
2021-11-03 13:33:34 +0800

    feat: add game page

---
+++ src/views/Game.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container bg-darkBlue">
+    <TheHeader />
+    <ProgressBar
+      :go-to-next-stage="goToNextStage"
+      :back-to-previous-stage="backToPreviousStage"
+      :whichStage="whichStage"
+    />
+    <GameContent :which-Question="whichStage" :go-to-next-stage="goToNextStage" />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue';
+import ProgressBar from '@/components/ProgressBar.vue';
+import GameContent from '@/components/GameContent.vue';
+
+export default {
+  name: 'TheTutorial',
+  data() {
+    return { whichStage: 1 };
+  },
+  methods: {
+    goToNextStage() {
+      if (this.whichStage < 5) {
+        this.whichStage += 1;
+      }
+    },
+    backToPreviousStage() {
+      if (this.whichStage > 1) {
+        this.whichStage -= 1;
+      }
+    },
+  },
+  components: {
+    TheHeader,
+    ProgressBar,
+    GameContent,
+  },
+  inject: ['isGamePage'],
+};
+</script>
