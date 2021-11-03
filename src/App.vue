978215e079234c4f62c3a8bcf64f3bf946b20aed
DotSea <85677992+DavidFu95@users.noreply.github.com>
2021-11-03 10:55:32 +0800

    feat: add boolean variable 'isGamePage' in App.vue I add the variable which is called 'isGamePage'. The usage of which is changing the style and icon when user entering tutorial page of game page.

--- src/App.vue
+++ src/App.vue
@@ -13,5 +13,23 @@ import TheFooter from './components/TheFooter.vue';
 
 export default {
   components: { TheFooter },
+  computed: {
+    isGamePage() {
+      return this.$route.name === 'Game';
+    },
+  },
+  provide() {
+    return {
+      isGamePage: () => this.isGamePage,
+    };
+  },
 };
 </script>
+<style scoped>
+button {
+  width: 200px;
+  height: 200px;
+  color: #000;
+  background-color: red;
+}
+</style>
