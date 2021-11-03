4aad00b50f5216c20b3927db80b880b6b37f6515
DotSea <85677992+DavidFu95@users.noreply.github.com>
2021-11-03 13:35:54 +0800

    feat: add router path of game page.

--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,14 @@ const routes = [
   {
     path: '/tutorial',
     name: 'Tutorial',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Tutorial.vue'),
+
+    component: () => import('../views/Tutorial.vue'),
+  },
+  {
+    path: '/game',
+    name: 'Game',
+
+    component: () => import('../views/Game.vue'),
   },
 ];
 
