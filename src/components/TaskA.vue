4baa0b6b9f88c14d5072fb32242c4d607e26fb27
DotSea <85677992+DavidFu95@users.noreply.github.com>
2021-12-29 18:50:57 +0800

    fix: code spelling wrong, rewirte it

--- src/components/TaskA.vue
+++ src/components/TaskA.vue
@@ -191,7 +191,10 @@ export default {
     try {
       // get factory id from database 'location'
       if (this.isGamePage()) {
-        if (localStorage.getItem('SpotDiffData') === null || localStorage.getItem.length !== 15) {
+        if (
+          localStorage.getItem('SpotDiffData') === null
+          || localStorage.getItem('SpotDiffData').length !== 15
+        ) {
           this.isLoading = true;
           await this.getFactoriesData();
           localStorage.setItem('SpotDiffDataDoneTime', 0);
